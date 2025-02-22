6748e79c804a43418763c13739b14d810af11d60
ChefOnanga <nangbiteghe@gmail.com>
2025-02-22 09:44:04 +0100

    v12

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,6 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export', // Génère un build statique pour Vercel
 };
 
-export default nextConfig;
+module.exports = nextConfig;
